9f869bfef83f3179a338d45f8d82ccb490db251f
Bobby Woolf <bwoolf1@users.noreply.github.com>
2020-04-10 15:54:09 -0400

    Improve the default description of Guide pages (#126)
    * Add video to office hours and article pages
    
    * ibm-garage-cloud/planning#260: Add intro
    
    * ibm-garage-cloud/planning#278 Better description

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const pathPrefix = process.env.PATH_PREFIX || '/';
 module.exports = {
   siteMetadata: {
     title: 'IBM Garage Cloud Native Toolkit',
-    description: 'Cloud Native Toolkit to enable development with IBM Cloud and RedHat',
+    description: 'Cloud Native Toolkit to enable development with IBM Cloud and Red Hat OpenShift',
     keywords: 'gatsby,theme,carbon',
   },
   pathPrefix,
