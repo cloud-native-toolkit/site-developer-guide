edb0854d405460457b54d50d6f17ec9c293c25b7
Sean Sundberg <seansund@us.ibm.com>
2021-02-24 10:10:10 -0600

    Updates to footer and global content
    - Updates license link in footer
    - Changes Copyright to Cloud-Native Toolkit Authors in footer
    - Makes "Last updated" based off of last build date in footer
    - Adds blanket licensing statement in footer
    - Removes IBM logo in footer
    - Changes site title and nav title to Cloud-Native Tookit
    - Changes `longName` in Globals to `Cloud-Native Toolkit` (e.g. removes `IBM` from title)
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -1,47 +1,102 @@
 import React from 'react';
-import Footer from 'gatsby-theme-carbon/src/components/Footer';
+import {graphql, useStaticQuery} from "gatsby";
+import {content, footer, grid, listItem, nav} from "gatsby-theme-carbon/src/components/Footer/Footer.module.scss";
+import {Column, Grid, Row} from "gatsby-theme-carbon/src/components/Grid";
+
+import {default as Globals} from '../templates/Globals';
 
 const currentYear = new Date().getFullYear();
-const lastUpdated = "October 2020";
 
-const version = "1.1.2";
-
-const Content = () => (
-    <>
-        <p>
-            Have questions? open
-            <br /> an issue on{' '}
-            <a
-                style={{ textDecoration: 'underline' }}
-                href="https://github.com/ibm-garage-cloud/planning/issues/new/choose">
-                GitHub.
-            </a>
-        </p>
-        <p>
-            Components version {version}
-            <br />
-            Last updated {lastUpdated}
-            <br />
-            Copyright © {currentYear} IBM
-        </p>
-    </>
-);
+const Content = ({buildTime}) => {
+    return (
+        <>
+            <p>
+                Have questions? open
+                <br /> an issue on{' '}
+                <a
+                    style={{ textDecoration: 'underline' }}
+                    href="https://github.com/ibm-garage-cloud/planning/issues/new/choose">
+                    GitHub.
+                </a>
+            </p>
+            <p>
+                Last updated {buildTime}
+            </p>
+            <p>
+                Copyright © {currentYear} Cloud-Native Toolkit Authors
+            </p>
+        </>
+    );
+}
 
 const links = {
     firstCol: [
         {
             href: '/contributing',
-            linkText: 'Contribute',
+            linkText: `Contribute to the ${Globals.get('longName')}`,
         },
-        { href: 'https://www.ibm.com/privacy', linkText: 'Privacy' },
-        { href: 'https://www.ibm.com/legal', linkText: 'Terms of use' },
-
-    ],
-    secondCol: [
-        { href: 'https://www.ibm.com/', linkText: 'IBM.com' }
+        {
+            href: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+            linkText: 'Site licensed under CC BY-NC-SA 4.0'
+        }
     ],
 };
 
-const CustomFooter = () => <Footer links={links} Content={Content} />;
+const LicensingDisclaimer = () => (
+    <>
+        Unless otherwise noted, the content of this site is licensed under
+        the <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0) License</a>,
+        and code samples are licensed under
+        the <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache 2.0 License</a>.
+    </>
+);
+
+const MyFooter = ({ Content, links, DisclaimerContent }) => {
+    const { firstCol, secondCol } = links;
+    const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        buildTime(formatString: "DD MMMM YYYY")
+      }
+    }
+  `);
+    return (
+        <footer className={footer}>
+            <Grid className={grid}>
+                <Row>
+                    <Column colLg={4} colMd={4}>
+                        <ul className={nav}>
+                            {firstCol &&
+                            firstCol.map((link, i) => (
+                                <li key={i} className={listItem} style={{ padding: '5px 0' }}>
+                                    <a href={link.href} aria-label={link.linkText}>
+                                        {link.linkText}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </Column>
+                    <Column
+                        className={content}
+                        colLg={4}
+                        colMd={4}
+                        colSm={3}
+                        offsetLg={2}>
+                        <Content buildTime={site.buildTime} />
+                    </Column>
+                </Row>
+                <Row>
+                    <Column
+                        className={content}
+                        colLg={12}>
+                        <DisclaimerContent />
+                    </Column>
+                </Row>
+            </Grid>
+        </footer>
+    );
+};
+
+const CustomFooter = () => <MyFooter links={links} Content={Content} DisclaimerContent={LicensingDisclaimer} />;
 
 export default CustomFooter;
